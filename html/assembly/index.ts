20db006196b11f963d7c60c21a27bab4fd8dc114
murata0531 <coless115@gmail.com>
2021-12-14 22:55:08 +0900

    円周率を求める関数を追加

--- html/assembly/index.ts
+++ html/assembly/index.ts
@@ -4,6 +4,20 @@ export function add(a: i32, b: i32): i32 {
   return a + b;
 }
 
-export function twice(x: i32) : i32 {
-  return x ** 10;
+export function twice(x: i32) : f32 {
+  let pi4:f32 = 0;
+  for(let i:i32 = 0;i < x;i++){
+    let float_i:f32 = i as f32;
+    pi4 += (1 / (float_i * 4 + 1) - 1 / (float_i * 4 + 3));
+  }
+  return pi4 * 4;
+}
+
+export function leibniz(x: i32) : f32 {
+  let pi4:f32 = 0;
+  for(let i:i32 = 0;i < x;i++){
+    let float_i:f32 = i as f32;
+    pi4 += (1 / (float_i * 4 + 1) - 1 / (float_i * 4 + 3));
+  }
+  return pi4 * 4;
 }
