8c5ecfcd8c8e3669ee98dededfc5e8981d9b1d9c
murata0531 <coless115@gmail.com>
2022-01-10 22:47:34 +0900

    ライプニッツの計算式を修正&ライプニッツ計算を20回行う

--- html/assembly/index.ts
+++ html/assembly/index.ts
@@ -4,20 +4,26 @@ export function add(a: i32, b: i32): i32 {
   return a + b;
 }
 
-export function twice(x: i32) : f32 {
-  let pi4:f32 = 0;
-  for(let i:i32 = 0;i < x;i++){
-    let float_i:f32 = i as f32;
-    pi4 += (1 / (float_i * 4 + 1) - 1 / (float_i * 4 + 3));
+export function twice(x: i32) : f64 {
+  let y:f64 = 0;
+  for(let cnt = 0;cnt < 10;cnt++){
+    let pi4:f64 = 0;
+    for(let i:i32 = 0;i < x;i++){
+        pi4 += (1 / (i * 4 + 1) - 1 / (i * 4 + 3));
+    }
+    y = pi4 * 4;
   }
-  return pi4 * 4;
+  return y;
 }
 
-export function leibniz(x: i32) : f32 {
-  let pi4:f32 = 0;
-  for(let i:i32 = 0;i < x;i++){
-    let float_i:f32 = i as f32;
-    pi4 += (1 / (float_i * 4 + 1) - 1 / (float_i * 4 + 3));
+export function leibniz(x: i32) : f64 {
+  let y:f64 = 0;
+  for(let cnt = 0;cnt < 20;cnt++){
+    let pi4:f64 = 0;
+    for(let i:i32 = 0;i < x;i++){
+      pi4 += (1 / (i * 4 + 1) - 1 / (i * 4 + 3));
+    }
+    y = pi4 * 4;
   }
-  return pi4 * 4;
+  return y;
 }
